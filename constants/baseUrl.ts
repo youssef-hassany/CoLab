cf8e84eb316878b952c46a4dc4e8ae985079735a
nightstalker5699 <94228182+nightstalker5699@users.noreply.github.com>
2025-08-10 01:24:52 +0300

    ready for production

--- constants/baseUrl.ts
+++ constants/baseUrl.ts
@@ -1,2 +1,4 @@
 export const baseUrl =
-  process.env.NODE_ENV === "production" ? "" : "http://localhost:8000";
+  process.env.NODE_ENV === "production"
+    ? "http://localhost:8000"
+    : "http://localhost:8000";
